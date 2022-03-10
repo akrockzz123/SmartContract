6c3b3f5688628b668984d3eac3fa58b12b64089d
Aniket Kumar <aniketkumar1601@gmail.com>
2022-03-10 08:44:09 +0530

    deploying the contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,32 +6,27 @@ const Web3 = require('web3') // constructor of web3
 
 const web3 = new Web3(ganache.provider()); // create instance of web3 and connect to etherium network using provider
 
+const { interface,bytecode } = require('../compile') 
 
 
-class Car {
-    park() {
-        return 'stopped';
-    }
 
-    drive() {
-        return 'vroom';
-    }
-}
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach(async () => {
     // get a list of accounts(just for sending and reciving ether)
 
     //web3 functions are asynchronous in nature
 
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts()
+      
 
         // use one of those accounts to deploy the contracts
-
-
+        // changing value of contract consumes gas
+        inbox = await new web3.eth.Contract(JSON.parse(interface))
+              .deploy({ data: bytecode, arguments: ['Hi there']})
+              .send({ from : accounts[0], gas: '1000000'})
 });
 
 // making each contracts
@@ -39,6 +34,7 @@ describe('Inbox', () => {
 
     it('deploys a contract', () => {
 
+        console.log(inbox);
 
     });
 })
